57237f4da81626d660904207d8bf1935d97614dd
YiJian.Ching@sas.com <YiJian.Ching@sas.com>
2022-08-13 03:38:48 +0000

    Commit Python_GradientBoosting__Python_&_SAS_Model_Pipeline_ /uaa/mmprojectpublic/GitHub Publishing

---
+++ Loan_Application_Project_7c800b0a-ad66-468a-809c-5421c4846f78/Version 1/Python_GradientBoosting__Python_&_SAS_Model_Pipeline_/example_test.py
@@ -0,0 +1,17 @@
+import os
+import os.path
+import sys
+
+sys.path.append('/models/resources/viya/18a81ad7-db11-4470-8a0b-fc3d807efce2/')
+
+import _dj5n7wg1ne74bjo4ln9lur5fz
+
+import settings_18a81ad7_db11_4470_8a0b_fc3d807efce2
+
+settings_18a81ad7_db11_4470_8a0b_fc3d807efce2.pickle_path = '/models/resources/viya/18a81ad7-db11-4470-8a0b-fc3d807efce2/'
+
+def score_record(Delinquencies,DerogatoryMarks,Inquiries,JobType,CredLineAge,CredLines,DebtIncRatio,LoanRequest,HomeValue,Income,LoanToValue,YearsOnJob):
+    "Output: P_Default0,P_Default1,I_Default"
+    return _dj5n7wg1ne74bjo4ln9lur5fz.score_method(Delinquencies,DerogatoryMarks,Inquiries,JobType,CredLineAge,CredLines,DebtIncRatio,LoanRequest,HomeValue,Income,LoanToValue,YearsOnJob)
+
+print(score_record(18.69,193.18,12.75,"",110.48,105.28,51.84,36.24,48.11,195.25,42.49,21.18))
