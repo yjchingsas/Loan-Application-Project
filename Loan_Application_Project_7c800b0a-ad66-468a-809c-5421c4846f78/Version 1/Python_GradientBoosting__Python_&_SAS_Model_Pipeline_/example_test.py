c6b2e8dc7a674710c0cbc3734c737d6ddf86cd69
YiJian.Ching@sas.com <YiJian.Ching@sas.com>
2022-08-26 03:05:37 +0000

    Commit Python_GradientBoosting__Python_&_SAS_Model_Pipeline_ /uaa/mmprojectpublic/GitHub Publishing

--- Loan_Application_Project_7c800b0a-ad66-468a-809c-5421c4846f78/Version 1/Python_GradientBoosting__Python_&_SAS_Model_Pipeline_/example_test.py
+++ Loan_Application_Project_7c800b0a-ad66-468a-809c-5421c4846f78/Version 1/Python_GradientBoosting__Python_&_SAS_Model_Pipeline_/example_test.py
@@ -14,4 +14,4 @@ def score_record(Delinquencies,DerogatoryMarks,Inquiries,JobType,CredLineAge,Cre
     "Output: P_Default0,P_Default1,I_Default"
     return _dj5n7wg1ne74bjo4ln9lur5fz.score_method(Delinquencies,DerogatoryMarks,Inquiries,JobType,CredLineAge,CredLines,DebtIncRatio,LoanRequest,HomeValue,Income,LoanToValue,YearsOnJob)
 
-print(score_record(89.19,75.47,24.89,"",4.49,56.99,68.54,178.03,65.74,52.8,101.35,43.1))
+print(score_record(62.99,73.28,78.83,"",105.95,45.18,182.59,12.66,18.11,128.78,7.06,71.83))
