bb8287e52923ad5a1669d04c5c4fbd7461795fbe
ssayjc <yijian.ching@sas.com>
2024-05-03 17:46:13 +0800

    dont want asynch publish

--- deploy_model_container.py
+++ deploy_model_container.py
@@ -54,9 +54,9 @@ print(payload)
 
 ##publish module 
 post_module_publish = requests.post(
-    url=f'{protocol}://{server}/modelManagement/publish',
+    url=f'{protocol}://{server}/modelPublish/models',
     headers={'Authorization': f'Bearer {access_token}',
-            'Content-Type': 'application/vnd.sas.models.publishing.request.asynchronous+json'},
+            'Content-Type': 'application/vnd.sas.models.publishing.request+json'},
     params=dict(force='True', reloadModelTable='True'),
     data=json.dumps(payload),
     verify=False
